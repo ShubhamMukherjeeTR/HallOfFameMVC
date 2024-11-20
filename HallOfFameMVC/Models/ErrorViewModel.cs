671d444c5dabe527692cbb538d78dc15b0be1ff2
Shubham Mukherjee <Shubham.Mukherjee@thomsonreuters.com>
2024-11-20 15:16:37 +0530

    Add project files.

---
+++ HallOfFameMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HallOfFameMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
